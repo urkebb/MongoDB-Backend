5c9151d01df1406073d60188d4241a23b856fa15
kopriva98 <66503247+kopriva98@users.noreply.github.com>
2021-02-12 16:02:55 +0100

    Backend Gotov
    Gotov Backend

--- MongoDB-Backend/controllers/users-controllers.js
+++ MongoDB-Backend/controllers/users-controllers.js
@@ -19,13 +19,13 @@ const getUsers = (req, res, next) => {
 const signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Nevalidan Input.', 422);
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
   }
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_USERS.find(u => u.email === email);
   if (hasUser) {
-    throw new HttpError('Vec postoji email', 422);
+    throw new HttpError('Could not create user, email already exists.', 422);
   }
 
   const createdUser = {
@@ -45,7 +45,7 @@ const login = (req, res, next) => {
 
   const identifiedUser = DUMMY_USERS.find(u => u.email === email);
   if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError('Lose informacije', 401);
+    throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
   }
 
   res.json({message: 'Logged in!'});
